956463d610b6887a369142d8a46acc861776ca0a
hanaapriliani13 <146050204+hanaapriliani13@users.noreply.github.com>
2023-12-26 22:07:09 +0700

    Add files via upload

---
+++ otentikasi.js
@@ -0,0 +1,18 @@
+const username = "hana"
+const password = "1"
+
+function auth(){
+    // tangkap nilai yang di input user
+    let userInput = document.getElementById("username").value
+    let passwordInput = document.getElementById("password").value
+
+    let form = document.getElementById('form')
+
+    if(username == userInput && password == passwordInput){
+       alert("Login Berhasil")
+       form.submit()
+       
+    } else {
+        alert("Login Gagal")
+    }
+}
